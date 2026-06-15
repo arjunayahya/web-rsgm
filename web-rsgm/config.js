f5a67e8ae96ea1e47dbefd0164958ed0c727f2e3
arjunayahya <arjunayahya.id@gmail.com>
2026-06-15 09:41:17 +0700

    Delete web-rsgm directory

--- web-rsgm/config.js
+++
@@ -1,7 +0,0 @@
-// Ganti dengan Project URL Anda dari Supabase
-const SUPABASE_URL = 'https://lkzplpejlwagavakmeep.supabase.co/rest/v1/'; 
-
-// Ganti dengan anon public key Anda dari Supabase
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrenBscGVqbHdhZ2F2YWttZWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjQ0ODIsImV4cCI6MjA5NzA0MDQ4Mn0.YgXbAFdCdqpjT6sEutw7wJwSwUWj9qFBtRvBniXCqiI'; 
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
